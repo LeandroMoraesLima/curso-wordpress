dbd6fc8cfae8a9b4ba656b54fd38ca82488b3be6
leandro <le.mlima@hotmail.com>
2019-07-09 22:01:55 -0300

    foi renderizado o sass

--- wp-content/themes/curso/functions.php
+++ wp-content/themes/curso/functions.php
@@ -1,7 +1,23 @@
 <?php 
+	define('IMG', get_template_directory_uri().'/assets/img');
+	define('CSS', get_template_directory_uri().'/assets/css');
+	define('JS', get_template_directory_uri().'/assets/js');
 
+// Função para carregamento dos scripts
 function carrega_scripts(){
-	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
-}
 
+	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
+	wp_enqueue_script( 'template', get_template_directory_uri() . '/js/template.js', array(), null, true);
+	wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', array(), '4.1.3', 'all');
+	wp_enqueue_script( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('jquery'), null, true);
+}
 add_action('wp_enqueue_scripts', 'carrega_scripts');
+
+// Função para menus
+register_nav_menus(
+	array(
+		'meu_menu_principal' => 'Menu Principal',
+		'menu_rodape' => 'Menu Rodapé'
+		
+	)
+);
--- wp-content/themes/curso/header.php
+++ wp-content/themes/curso/header.php
@@ -6,17 +6,24 @@
 
 	<?php wp_head(); ?>
 
+	<link rel="stylesheet" href="<?php echo CSS; ?>/app.css?v=1">
+
 </head>
 <body <?php body_class(); ?>>
-	<p>Este é um parágrafo</p>
 	
 <header>
-	<div class="barra-topo">
-		<div class="redes-sociais">Redes Sociais</div>
-		<div class="pesquisa">Pesquisa</div>
+	<div class="barra-topo container">
+		<div class="row">
+			<div class="redes-sociais col-md-4">Redes Sociais</div>
+			<div class="pesquisa col-md-8">Pesquisa</div>
+		</div>
 	</div>
-	<div class="area-menu">
-		<div class="logo">Logo</div>
-		<div class="menu-principal">Menu</div>
+	<div class="area-menu container">
+		<div class="row">
+			<div class="logo col-md-3">Logo</div>
+			<div class="menu-principal col-md-9 text-right">
+				<?php wp_nav_menu( array( 'theme_location' => 'meu_menu_principal')); ?>
+			</div>
+		</div>
 	</div>
-</header>
+</header> 
