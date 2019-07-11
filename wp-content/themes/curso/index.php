e2b4f1db6e17b799422345756e5ca30a21470ef4
leandro <le.mlima@hotmail.com>
2019-07-11 02:58:08 -0300

    foi criado as paginas page-home e paginageral (Template Name)

--- wp-content/themes/curso/index.php
+++ wp-content/themes/curso/index.php
@@ -9,10 +9,10 @@
 				<aside class="barra-lateral col-md-3">Barra Lateral</aside>
 				<div class="noticias col-md-9">
 					<?php
-						//Se houver algum post
-						if (have_posts()) :
-							//Enquanto houver algum post, chame o post de determinada maneira
-							while (have_posts()) : the_post();
+					//Se houver algum post
+					if (have_posts()) :
+						//Enquanto houver algum post, chame o post de determinada maneira
+						while (have_posts()) : the_post();
 					?>
 					<h1><?php the_title(); ?></h1>
 					<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
@@ -20,12 +20,12 @@
 					<p><?php the_tags('Tags: ', ', '); ?></p>
 					<p><?php the_content(); ?></p>
 					<?php
-						endwhile;
-						else: 
+					endwhile;
+					else: 
 					?>
 					<p>Não tem nada ainda para mostrar</p>
 					<?php
-						endif; 
+					endif; 
 					?>
 				</div>
 			</div>
---
+++ wp-content/themes/curso/paginageral.php
@@ -0,0 +1,32 @@
+<?php 
+/*
+Template Name: Páginas Gerais
+*/	
+?>
+<?php get_header(); ?>
+
+<div class="conteudo-wrapper">
+	<main>		
+		<div class="conteudo container">
+			<?php
+			//Se houver algum post
+			if (have_posts()) :
+				//Enquanto houver algum post, chame o post de determinada maneira
+				while (have_posts()) : the_post();
+			?>
+			<h1><?php the_title(); ?></h1>
+			<p>Autor <?php the_author(); ?></p>
+			<p><?php the_content(); ?></p>
+			<?php
+			endwhile;
+			else: 
+			?>
+			<p>Não tem nada ainda para mostrar</p>
+			<?php
+			endif; 
+			?>		
+		</div>
+	</main>
+</div>
+
+<?php get_footer(); ?>
